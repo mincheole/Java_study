1240d3e999ff03544b2ee64aa6fbee940bc14d02
Mincheol Kim <106231571+mincheole@users.noreply.github.com>
2024-03-24 22:26:00 +0900

    연습문제 4-8 완료
    연습문제 4-8 완료

--- src/JAVA_Programming_4th/Ex4_8.java
+++ src/JAVA_Programming_4th/Ex4_8.java
@@ -5,72 +5,76 @@ import java.util.Scanner;
 class Phone {
 	private String name;
 	private String tel;
-	
-	Phone() {}
-	
-	void setPhone(String name, String num) {
+
+	Phone() {
+	}
+
+	Phone(String name, String num) {
 		this.name = name;
 		this.tel = num;
 	}
-	
+
 	String getName() {
 		return name;
 	}
-	
+
 	String getTel() {
-		return tel;		
+		return tel;
 	}
 }
 
 class PhoneBook {
 	Scanner sc = new Scanner(System.in);
 	Phone[] p;
-	int people;
-	boolean state = true;
-	
-	PhoneBook() {}
-	PhoneBook(int num) {
-		people = num;
-		this.p = new Phone[people];
-		for(int i=0; i<num; i++) p[i] = new Phone();
+	int num;
+	String name;
+	String tel;
+
+	PhoneBook() {
 	}
-	
-	void setPhonebook(int i) {
-		System.out.print("이름과 전화번호(이름과 번호는 빈 칸 없이 입력)>>");
-		String s = sc.next();
-		String name= s.substring(0,2);
-		String num = s.substring(3);
-//		for(int i=0; i<people; i++) {
-		this.p[i].setPhone(name,num);
-//		}
+
+	void getNum() {
+		System.out.print("인원수>>");
+		num = sc.nextInt();
+		this.p = new Phone[num];
 	}
-	void searchPhone(int i) {
-		System.out.print("검색할 이름>>");
-		String searchName = sc.next();
-		if(searchName.equals("그만")) state = false;
-		else if(p[i].getName().equals(searchName))
-//		else if(p[i].getName()==searchName)
-			System.out.println(p[i].getName()+"의 번호는 " +p[i].getTel()+ " 입니다.");
-		System.out.println(searchName+" 이/가 없습니다.");
-	}
-	void run() {
-		for(int i=0; i<this.people; i++) {
-			setPhonebook(i);
+
+	void setPhonebook() {
+		for (int i = 0; i < num; i++) {
+			System.out.print("이름과 전화번호(이름과 번호는 빈 칸 없이 입력)>>");
+			name = sc.next();
+			tel = sc.next();
+			p[i] = new Phone(name, tel);
 		}
 		System.out.println("저장되었습니다...");
-		while(state)
-			for(int i=0; i<this.people; i++) {
-				searchPhone(i);
+	}
+
+	void searchPhone() {
+		boolean state = true;
+		while (true) {
+			System.out.print("검색할 이름>>");
+			String name = sc.next();
+			if (name.equals("그만")) {
+				System.out.println("프로그램을 종료합니다.");
+				break;
+			}
+
+			for (int i = 0; i < this.num; i++) {
+				if (p[i].getName().equals(name)) {
+					System.out.println(p[i].getName() + "의 번호는 " + p[i].getTel() + " 입니다.");
+				}
+				if (state == false)
+					System.out.println(name + " 이/가 없습니다.");
+			}
 		}
 	}
 }
 
 public class Ex4_8 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("인원수>>");
-		int n = sc.nextInt();
-		PhoneBook p = new PhoneBook(n);
-		p.run();
+		PhoneBook p = new PhoneBook();
+		p.getNum();
+		p.setPhonebook();
+		p.searchPhone();
 	}
 }
